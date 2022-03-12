ef47a500959d091c36867f98a83252936b6a6b78
Vjeko <vjeko.pavicin@gmail.com>
2022-03-12 23:13:25 +0100

    argh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Header: React.FC = () => (
 	<header>
-		<div>
-			<Link to="/">Hello</Link>
-			<Link to="/world">World</Link>
-		</div>
+		<Link to="/">Hello</Link>
+		<Link to="/world">World</Link>
 	</header>
 )
